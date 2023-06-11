71112932f03b9d74ea6838c6218e5aaa1d4abadd
Biuksum <midoymi@gmail.com>
2023-06-11 14:18:13 +0100

    Registro do Usuário na Base de Dados Firebase Imagem de Perfil, Nome, Email, Palavra Passe, Confirmar Palavra

--- app/src/main/java/com/example/ipgprojfinal/activities/MainActivity.java
+++ app/src/main/java/com/example/ipgprojfinal/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.ipgprojfinal;
+package com.example.ipgprojfinal.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.ipgprojfinal.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/ipgprojfinal/activities/SignInActivity.java
+++ app/src/main/java/com/example/ipgprojfinal/activities/SignInActivity.java
@@ -1,15 +1,11 @@
 package com.example.ipgprojfinal.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ipgprojfinal.databinding.ActivitySignInBinding;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -26,21 +22,7 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
-        binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
     }
 
-    private void addDataToFirestore(){
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("first_name", "Biuksum");
-        data.put("last_name", "Antonio Sina");
-        database.collection("users")
-                .add(data)
-                .addOnSuccessListener(documentReference -> {
-                    Toast.makeText(getApplicationContext(),"Data Inserted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(exeption ->{
-                    Toast.makeText(getApplicationContext(),exeption.getMessage(),Toast.LENGTH_SHORT).show();
-                });
-    }
+
 }
